6fef5a2414683b1c66d5acb8b3ccb7986399e25a
Daniel Wahl <daniel.wahl@masonohioschools.com>
2017-10-28 00:19:24 +0000

    Add personal styling to website

---
+++ Projects/BreadDodge/game.js
@@ -0,0 +1,17 @@
+var player;
+function setup() {
+    createCanvas(250, 250);
+    player = createSprite(width/2, height -25, 50, 50);
+}
+
+function draw() {
+    background(153,170,181);
+    drawSprites();
+    
+    if (keyDown(RIGHT_ARROW)) {
+    player.position.x = player.position.x + 1;
+  }
+  if (keyDown(LEFT_ARROW)) {
+    player.position.x = player.position.x - 1;
+  }
+}
